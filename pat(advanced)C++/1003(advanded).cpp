10e032ab1ff9d06f8f94ed6b28d27b77d92338da
Xnaivety <827725022@qq.com>
2020-08-01 15:45:50 +0800

    pat(advanced)C++
    pat(advanced)C++

--- pat(advanced)C++/1003(advanded).cpp
+++ pat(advanced)C++/1003(advanded).cpp
@@ -1,47 +1,77 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+
 using namespace std;
-int n, m, c1, c2;//n为城市的数量，m为路径数量，c1是出发地，c2是目的地
-int e[510][510], weight[510], dis[510], num[510], w[510];//e为邻接矩阵，记录任意两个城市之间的距离，weight数组是每个城市搜救队伍的数量，dis数组为出发点到该点的最短距离，num数组为出发点到该点最短路径的条数，w数组为出发点到该点最短路径下的最多救援队个数
-bool visit[510];//该数组未经过初始化？？？可能是默认true//vist数组表示是否被选入最短路径
-const int inf = 99999999;
-int main() {
-    scanf("%d%d%d%d", &n, &m, &c1, &c2);//遍历赋值第一行数据
-    for(int i = 0; i < n; i++)//遍历并赋值第二行数据
-        scanf("%d", &weight[i]);
-    fill(e[0], e[0] + 510 * 510, inf);//将数组e的510*510个单元格全部啊赋值成inf
-    fill(dis, dis + 510, inf);//将距离数组全部赋值为无穷
-    int a, b, c;//a,b为城市，c为a，b两个城市之间的距离
-    for(int i = 0; i < m; i++) {遍历赋值给数组e
-        scanf("%d%d%d", &a, &b, &c);
-        e[a][b] = e[b][a] = c;//因为是无向图，因此邻接矩阵关于斜对角对称
-    }
-    dis[c1] = 0;//将出发地的距离赋值为0
-    w[c1] = weight[c1];//出发点并入最短路径，并将救援队数量加入总和
-    num[c1] = 1;//初始路径数量为1
-    for(int i = 0; i < n; i++) {//该for循环选出未被并入最短路径的结点
-        int u = -1,   = inf;
-        for(int j = 0; j < n; j++) {
-            if(visit[j] == false && dis[j] < minn) {
-                u = j;
-                minn = dis[j];//
-            }
-        }
-        if(u == -1) break;//判断是否有新结点并入最短路径，如果没有则退出循环，打印结果
-        for(int v = 0; v < n; v++) {//遍历未被并入最短路径的结点，刷新各个数值
-            if(visit[v] == false && e[u][v] != inf) {
-                if(dis[u] + e[u][v] < dis[v]) {//距离更短的情况下，刷新距离以及救援队数量
-                    dis[v] = dis[u] + e[u][v];
-                    num[v] = num[u];
-                    w[v] = w[u] + weight[v];
-                } else if(dis[u] + e[u][v] == dis[v]) {//距离长短一样的情况下，比较救援队数量，如果更少就刷新
-                    num[v] = num[v] + num[u];
-                    if(w[u] + weight[v] > w[v])
-                        w[v] = w[u] + weight[v];
+
+
+int distances[500][500];
+int number_of_city;
+int number_of_roads;
+int startcity;
+int endcity;
+int visit[500] = {0};
+int mindis[500] = {0};
+int number_of_rescue_teams[500];
+int rescuenumber[500];
+int number_count[500] = {0};
+
+void dijkstra(int indexcity) {
+    while (indexcity != endcity) {
+        for (int i = 0; i < number_of_city; i++) {
+            if (visit[i] == 0 && distances[indexcity][i] != 999999) {
+                if (mindis[i] > mindis[indexcity] + distances[i][indexcity]) {
+                    number_count[i] = number_count[indexcity];
+                    mindis[i] = mindis[indexcity] + distances[i][indexcity];
+                    rescuenumber[i] = rescuenumber[indexcity] + number_of_rescue_teams[i];
+                } else if (mindis[i] == mindis[indexcity] + distances[i][indexcity]) {
+                    number_count[i] += number_count[indexcity];
+                    if (rescuenumber[i] < rescuenumber[indexcity] + number_of_rescue_teams[i]) {
+                        rescuenumber[i] = rescuenumber[indexcity] + number_of_rescue_teams[i];
+                    }
                 }
             }
         }
+        int mincity = 500;
+        int mindiss = 999999;
+        for (int j = 0; j < number_of_city; j++) {
+            if (visit[j] == 0 && mindis[j] < mindiss) {
+                mincity = j;
+                mindiss = mindis[j];
+            }
+        }
+        visit[mincity] = 1;
+        indexcity = mincity;
     }
-    printf("%d %d", num[c2], w[c2]);
-    return 0;
+}
+
+
+int main() {
+    cin >> number_of_city >> number_of_roads >> startcity >> endcity;
+    for (int i = 0; i < number_of_city; i++) {
+        cin >> number_of_rescue_teams[i];
+    }
+    for (int j = 0; j < number_of_city; j++) {
+        for (int i = 0; i < number_of_city; i++) {
+            distances[j][i] = 999999;
+        }
+    }
+
+
+    for (int i = 0; i < number_of_roads; i++) {
+        int city1;
+        int city2;
+        int index_distance;
+        cin >> city1 >> city2 >> index_distance;
+        distances[city1][city2] = distances[city2][city1] = index_distance;
+    }
+
+    for (int i = 0; i < number_of_city; i++) {
+        mindis[i] = 999999;
+    }
+
+    visit[startcity] = 1;
+    mindis[startcity] = 0;
+    rescuenumber[startcity] = number_of_rescue_teams[startcity];
+    number_count[startcity] = 1;
+    dijkstra(startcity);
+    cout << number_count[endcity] << ' ' << rescuenumber[endcity];
 }
